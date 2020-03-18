731e16ff96518591a90a1f61d795a3f049869792
Fabio Cimmino <fabio-cimmino@outlook.it>
2020-03-18 17:10:34 +0100

    Progetto iniziale aggiunto nel branch Spirnt_A

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+import com.backendless.Backendless;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        User p1 = new User();
+        p1.setName("Fabio");
+        p1.setSurname("Steve");
+
+        Backendless.Data.of(User.class).save(p1, new AsyncCallback<User>() {
+                    @Override
+                    public void handleResponse(User response) {
+
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+
+                    }
+                }
+        );
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/TestApplication.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.app.Application;
+
+import com.backendless.Backendless;
+
+public class TestApplication extends Application {
+    public static final String APPLICATION_ID = "CC93AEC0-074E-BCAE-FFFE-748FA924E000";
+    public static final String API_KEY = "278AD5E3-D39E-49EA-ACB4-495652BF6203";
+    public static final String SERVER_URL = "https://api.backendless.com";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Backendless.setUrl( SERVER_URL );
+        Backendless.initApp( getApplicationContext(),
+                APPLICATION_ID,
+                API_KEY );
+    }
+
+}
